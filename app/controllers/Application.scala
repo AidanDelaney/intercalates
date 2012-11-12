8f32eeabb7f6ca78de7072bed211e20c0711a8fb
Aidan Delaney <aidan@phoric.eu>
2012-11-12 20:11:46 +0000

    Factor out drawing of passed Json object

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,11 +8,17 @@ object Application extends Controller {
 
   def index = Action { request =>
     request.contentType match {
-      case Some("application/json") => Ok(Json.toJson(Map("Result" -> true)))
+      case Some("application/json") => doDrawing(request.body.asJson)
       case Some("text/html")        => Ok(views.html.index("Your new application is ready."))
       case Some(reqType)            => BadRequest("Requested type " + reqType + " was not recognised")
       case None                     => BadRequest("You must specify a content-type in the request")
     }
   }
-  
+
+  def doDrawing (json: Option [JsValue]) = {
+    json match {
+      case Some(o) => Ok(Json.toJson(Map("Result" -> true)))
+      case None    => BadRequest("No Json object passed to draw");
+    }
+  }
 }
