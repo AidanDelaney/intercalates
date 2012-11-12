8af18db9b69c65f7670da7962964d177be93dd84
Aidan Delaney <aidan@phoric.eu>
2012-11-12 19:56:14 +0000

    Correctly handle JSON or HTML content-type

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+  def index = Action { request =>
+    request.contentType match {
+      case Some("application/json") => Ok(Json.toJson(Map("Result" -> true)))
+      case Some("text/html")        => Ok(views.html.index("Your new application is ready."))
+      case Some(reqType)            => BadRequest("Requested type " + reqType + " was not recognised")
+      case None                     => BadRequest("You must specify a content-type in the request")
+    }
   }
   
 }
